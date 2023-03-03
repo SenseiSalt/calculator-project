531e23d94f1c076e51b838588b4e260658f9ad5c
SenseiSalt <thebeckster603@gmail.com>
2023-03-03 08:28:48 -0500

    Changed color of function buttons
    added function that prints numbers to display when clicked

--- function.js
+++ function.js
@@ -1,1 +1,15 @@
-numButtons = document.querySelectorAll('');
+const numButtons = document.querySelectorAll('.num');
+const display = document.querySelector('.screen-text');
+display.innerText = "";
+
+numButtons.forEach(button => button.addEventListener('click', getWhichNumButton));
+
+function getWhichNumButton(e) {
+    let buttonData = e.target.getAttribute("data-key");
+    console.log(display.innerText.length)
+    if (display.innerText.length < 12) {
+        display.innerText += buttonData;
+    }
+
+
+}
